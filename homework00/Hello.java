d208487b3361cb955e47d4aa1ba043974e77bb27
ethanb10001010011 <35483381+ethanb10001010011@users.noreply.github.com>
2018-01-16 00:54:34 -0800

    Final edits to Hello.java for now!

--- homework00/Hello.java
+++ homework00/Hello.java
@@ -1,4 +1,21 @@
-public class HelloWorld {
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  Hello.java
+ *  Purpose       :  Prints "Hello, world!"
+ *  Author        :  Ethan Boone
+ *  Date          :  2018-01-16
+ *  Description   :  Prints "Hello, world!"
+ *  Notes         :  None
+ *  Warnings      :  None
+ *  Exceptions    :  None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision History
+ *  ---------------
+ *            Rev      Date     Modified by:  Reason for change/modification
+ *           -----  ----------  ------------  -----------------------------------------------------------
+ *  @version 1.0.0  2017-01-16  Ethan Boone Initial writing and release
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+public class Hello {
   public static void main ( String [] args ) {
     System.out.println( "Hello, world!" );
   }
