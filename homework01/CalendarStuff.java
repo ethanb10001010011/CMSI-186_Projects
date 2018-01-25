73a43d5eca7b413ba0bc0935a6a26e598ae2e8b6
ethanb10001010011 <35483381+ethanb10001010011@users.noreply.github.com>
2018-01-25 09:14:17 -0800

    Almost finished homework #01!

--- homework01/CalendarStuff.java
+++ homework01/CalendarStuff.java
@@ -3,8 +3,8 @@
  *  Purpose       :  Provides a class with supporting methods for CountTheDays.java program
  *  Author        :  B.J. Johnson (prototype)
  *  Date          :  2017-01-02 (prototype)
- *  Author        :  <your name here>
- *  Date          :  <date of writing here>
+ *  Author        :  <Ethan Boone>
+ *  Date          :  <2018.01.23>
  *  Description   :  This file provides the supporting methods for the CountTheDays program which will
  *                   calculate the number of days between two dates.  It shows the use of modularization
  *                   when writing Java code, and how the Java compiler can "figure things out" on its
@@ -39,7 +39,7 @@ public class CalendarStuff {
    * A listing of the months of the year, assigning numbers; I suppose these could be ENUMs instead, but whatever
    */
    private static final int JANUARY    = 0;
-   private static final int FEBRUARY   = JANUARY   + 1
+   private static final int FEBRUARY   = JANUARY   + 1;
    private static final int MARCH   = FEBRUARY   + 1;
    private static final int APRIL   = MARCH   + 1;
    private static final int MAY   = APRIL   + 1;
@@ -56,7 +56,8 @@ public class CalendarStuff {
    *  NOTE: this excludes leap years, so those will be handled as special cases
    *  NOTE: this is optional, but suggested
    */
-   private static int[]    days        = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+   private static long[]    normalDays        = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+   private static long[]    leapYearDays        = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
   /**
    * A method to determine if the year argument is a leap year or not<br />
@@ -85,59 +86,12 @@ public class CalendarStuff {
    public static long daysInMonth( long month, long year ) {
     long numDays = 0;
       if ( isLeapYear(year) ) {
-        if ( month == 0 ) {
-          numDays = 31;
-        } else if ( month == 1 ) {
-          numDays = 29;
-        } else if ( month == 2 ) {
-          numDays = 31;
-        } else if ( month == 3 ) {
-          numDays = 30;
-        } else if ( month == 4 ) {
-          numDays = 31;
-        } else if ( month == 5 ) {
-          numDays = 30;
-        } else if ( month == 6 ) {
-          numDays = 31;
-        } else if ( month == 7 ) {
-          numDays = 31;
-        } else if ( month == 8 ) {
-          numDays = 30;
-        } else if ( month == 9 ) {
-          numDays = 31;
-        } else if ( month == 10 ) {
-          numDays = 30;
-        } else if ( month == 11 ) {
-          numDays = 31;
-        }
+        numDays = leapYearDays[(int) month - 1];
       } else {
-        if ( month == 0 ) {
-          numDays = 31;
-        } else if ( month == 1 ) {
-          numDays = 28;
-        } else if ( month == 2 ) {
-          numDays = 31;
-        } else if ( month == 3 ) {
-          numDays = 30;
-        } else if ( month == 4 ) {
-          numDays = 31;
-        } else if ( month == 5 ) {
-          numDays = 30;
-        } else if ( month == 6 ) {
-          numDays = 31;
-        } else if ( month == 7 ) {
-          numDays = 31;
-        } else if ( month == 8 ) {
-          numDays = 30;
-        } else if ( month == 9 ) {
-          numDays = 31;
-        } else if ( month == 10 ) {
-          numDays = 30;
-        } else if ( month == 11 ) {
-          numDays = 31;
-        }
+        numDays = normalDays[(int) month - 1];
       }
-    }
+    return numDays;
+  }
 
   /**
    * A method to determine if two dates are exactly equal
@@ -214,16 +168,40 @@ public class CalendarStuff {
    *         be decremented to make the appropriate index value
    */
    public static boolean isValidDate( long month, long day, long year ) {
-      return true;  // replace this with the actual code
+    long inputMonth = month - 1;
+      if ( isLeapYear(year) == false && inputMonth <= 11 && inputMonth >= 0 && day > 0 && day <= normalDays[(int) inputMonth] ) {
+        return true;
+      } else if ( isLeapYear(year) == true && inputMonth <= 11 && inputMonth >= 0 && day > 0 && day <= leapYearDays[(int) inputMonth] ) {
+        return true;
+      } else {
+        return false;
+      }
    }
+
+
+
 
   /**
    * A method to return a string version of the month name
    * @param    month long   containing month number, starting with "1" for "January"
    * @return         String containing the string value of the month (no spaces)
    */
+      
+
    public static String toMonthString( int month ) {
       switch( month - 1 ) {
+        case JANUARY: return "January";
+        case FEBRUARY: return "February";
+        case MARCH: return "March";
+        case APRIL: return "April";
+        case MAY: return "May";
+        case JUNE: return "June";
+        case JULY: return "July";
+        case AUGUST: return "August";
+        case SEPTEMBER: return "September";
+        case OCTOBER: return "October";
+        case NOVEMBER: return "November";
+        case DECEMBER: return "December";
          default: throw new IllegalArgumentException( "Illegal month value given to 'toMonthString()'." );
       }
    }
@@ -235,9 +213,41 @@ public class CalendarStuff {
    */
    public static String toDayOfWeekString( int day ) {
       switch( day - 1 ) {
-         default       : throw new IllegalArgumentException( "Illegal day value given to 'toDayOfWeekString()'." );
+        case MONDAY: return "Monday";
+        case TUESDAY: return "Tuesday";
+        case WEDNESDAY: return "Wednesday";
+        case THURSDAY: return "Thursday";
+        case FRIDAY: return "Friday";
+        case SATURDAY: return "Saturday";
+        case SUNDAY: return "Sunday";
+        default : throw new IllegalArgumentException( "Illegal day value given to 'toDayOfWeekString()'." );
       }
    }
+
+
+
+   public static boolean withinAYear( long month1, long day1, long year1, long month2, long day2, long year2 ) {
+     if ( compareDate( month1, day1, year1, month2, day2, year2 ) == 0 ) {
+       return true;
+     } else if( compareDate( month1, day1, year1, month2, day2, year2 ) == 1 ) {
+         if( (year1 - year2 <= 1 && month1 < month2) || (year1 - year2 <= 1 && month1 <= month2 && day1 < day2) || (year1 == year2) ){
+           return true;
+         } else {
+          return false;
+         }
+     } else if( compareDate( month1, day1, year1, month2, day2, year2 ) == -1 ) {
+         if( (year2 - year1 <= 1 && month2 < month1) || (year2 - year1 <= 1 && month1 >= month2 && day1 > day2) || (year1 == year2) ) {
+           return true;
+         } else {
+          return false;
+         }
+     } else {
+         return false;
+     }
+   }
+
+
+
 
   /**
    * A method to return a count of the total number of days between two valid dates
@@ -251,8 +261,111 @@ public class CalendarStuff {
    */
    public static long daysBetween( long month1, long day1, long year1, long month2, long day2, long year2 ) {
       long dayCount = 0;
-      return dayCount;
+      long largerDay = 0;
+      long largerMonth = 0;
+      long largerYear = 0;
+      long monthsBetween = 0;
+      long leftoverDays = 0;
+      long yearsBetween = 0;
+      long numLeapYears = 0;
+
+      for( int i = (int) Math.min(year1, year2); i <= Math.max(year1, year2); i++) {
+        if(isLeapYear(i) == true) {
+          numLeapYears++;
+        }
+      }
+
+
+      if ( compareDate( month1, day1, year1, month2, day2, year2 ) == 0 ) {
+        return 0;
+      } else if ( compareDate( month1, day1, year1, month2, day2, year2 ) == 1 ) {
+        largerDay = day1;
+        largerMonth = month1;
+        largerYear = year1;
+        if( withinAYear( largerMonth, largerDay, largerYear, month2, day2, year2 ) == true ) {
+          if( largerYear - year2 == 1 && largerDay == day2 && largerMonth == month2) {
+            dayCount += 365;
+          } else {
+            if( ( Math.abs(largerMonth - month2) ) < 1 ) {
+              dayCount += ( largerDay - day2 );
+            } else {
+                for( int i = (int) month2 + 1; i < largerMonth; i++ ) {
+                dayCount += normalDays[i];
+            }
+                leftoverDays = (( normalDays[(int) month2] - day2 ) + day1 );
+                dayCount += leftoverDays;
+
+            }
+          }
+
+        }
+
+
+      } else if ( compareDate( month1, day1, year1, month2, day2, year2 ) == -1 ) {
+          largerDay = day2;
+          largerMonth = month2;
+          largerYear = year2;
+          if ( withinAYear( largerMonth, largerDay, largerYear, month1, day1, year1 ) == true ) {
+            if ( largerYear - year1 == 1 && largerDay == day1 && largerMonth == month1 ) {
+              dayCount += 365;
+            } else {
+                if ( ( Math.max(month1, month2) - Math.min(month1, month2) ) == 0 && ( year1 == year2 ) ) {
+                  dayCount += ( Math.max(day2, day1) - Math.min(day2, day1) );
+              } else if ( ( Math.max(month1, month2) - Math.min(month1, month2) ) == 0 && ( year1 != year2 ) ){
+                  dayCount += ( 365 - (Math.max(day2, day1) - Math.min(day2, day1)) );
+              } else if( ( Math.max(month1, month2) - Math.min(month1, month2) ) != 0 && ( year1 == year2 ) ) {
+                  for( int i = (int) Math.min(month1, month2) + 1; i < Math.max(month1, month2); i++ ) {
+                    dayCount += normalDays[i];
+                  }
+                  dayCount += largerDay + ( normalDays[(int) month1] - day1 );
+              } else {
+                  for( int i = (int) Math.min(month1, month2) + 1; i < Math.max(month1, month2); i++ ) {
+                    dayCount -= normalDays[i];
+                  }
+                  leftoverDays = (( normalDays[(int) month2] - day2 ) + day1 );
+                  dayCount -= leftoverDays;
+                  dayCount += 365;
+              }
+            } 
+        } else {
+            yearsBetween = Math.max(year1, year2) - Math.min(year1, year2);
+            year2 = year1;
+            dayCount += 365 * yearsBetween;
+            if ( ( Math.max(month1, month2) - Math.min(month1, month2) ) == 0) {
+                  dayCount += ( Math.max(day2, day1) - Math.min(day2, day1) );
+            } else if( ( Math.max(month1, month2) - Math.min(month1, month2) ) != 0) {
+                  for( int i = (int) Math.min(month1, month2) + 1; i < Math.max(month1, month2); i++ ) {
+                    dayCount += normalDays[i];
+                  }
+                  dayCount += largerDay + ( normalDays[(int) month1] - day1 );
+            }/* else {
+                for( int i = (int) Math.min(month1, month2) + 1; i < Math.max(month1, month2); i++ ) {
+                  dayCount -= normalDays[i];
+                }
+                  leftoverDays = (( normalDays[(int) month2] - day2 ) + day1 );
+                  dayCount -= leftoverDays;
+                  dayCount += 365;
+            }
+         */ }
+        }
+       return dayCount + numLeapYears;
    }
+  }
+/*
+days between function
 
-}
+use compareDate method to determine which date is larger
+if they are the same, return 0
+if not, use withinAYear function to figure out how many years they're apart
+if they're within a year
+- find gap in months (for statement starting with i=lower month, ending with i=high month) adding together the 
+- for months >lower month and <high month, add days together
+- subtract the total days of low month and days input for lower month
+- add that solution to number of days input for larger month
+- 
 
+check how many years the two dates are apart
+check how many months they are apart
+
+USE COMPAREDATE METHOD
+*/
